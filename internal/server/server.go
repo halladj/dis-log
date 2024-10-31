e313f5af1990e32251622f4c1d0f204d02527c44
halladj <hambz@arch.localdomain>
2024-10-31 18:40:16 +0100

    Added basic security ssl

--- internal/server/server.go
+++ internal/server/server.go
@@ -105,9 +105,12 @@ func (s *grpcServer) ConsumeStream(
 	}
 }
 
-func NewGRPCServer(config *Config) (*grpc.Server, error) {
+func NewGRPCServer(
+	config *Config,
+	opts ...grpc.ServerOption,
+) (*grpc.Server, error) {
 
-	gsrv := grpc.NewServer()
+	gsrv := grpc.NewServer(opts...)
 	srv, err := newgrpcServer(config)
 	if err != nil {
 		return nil, err
--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -7,9 +7,11 @@ import (
 	"testing"
 
 	api "github.com/halladj/dis-log/api/v1"
+	"github.com/halladj/dis-log/internal/config"
 	"github.com/halladj/dis-log/internal/log"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/status"
 )
 
@@ -43,17 +45,41 @@ func setupTest(t *testing.T, fn func(*Config)) (
 ) {
 	t.Helper()
 
-	l, err := net.Listen("tcp", ":0")
+	l, err := net.Listen("tcp", "127.0.0.1:0")
 	require.NoError(t, err)
 
-	clientOptions := []grpc.DialOption{
-		grpc.WithInsecure(),
-	}
-	cc, err := grpc.Dial(
-		l.Addr().String(),
-		clientOptions...,
+	clientTLSConfig, err := config.SetupTLSConfig(
+		config.TLSConfig{
+			CertFile: config.ClientCertFile,
+			KeyFile:  config.ClientKeyFile,
+			CAFile:   config.CAFile,
+		},
 	)
 	require.NoError(t, err)
+
+	clientCreds := credentials.NewTLS(
+		clientTLSConfig)
+	cc, err := grpc.Dial(
+		l.Addr().String(),
+		grpc.WithTransportCredentials(
+			clientCreds,
+		),
+	)
+	require.NoError(t, err)
+	client = api.NewLogClient(cc)
+
+	serverTLSConfig, err := config.SetupTLSConfig(
+		config.TLSConfig{
+			CertFile:      config.ServerCertFile,
+			KeyFile:       config.ServerKeyFile,
+			CAFile:        config.CAFile,
+			ServerAddress: l.Addr().String(),
+			Server:        true,
+		},
+	)
+	require.NoError(t, err)
+	serverCreds := credentials.NewTLS(
+		serverTLSConfig)
 
 	dir, err := os.MkdirTemp("", "server-test")
 	require.NoError(t, err)
@@ -68,19 +94,19 @@ func setupTest(t *testing.T, fn func(*Config)) (
 		fn(cfg)
 	}
 
-	server, err := NewGRPCServer(cfg)
+	server, err := NewGRPCServer(
+		cfg, grpc.Creds(serverCreds),
+	)
 	require.NoError(t, err)
 
 	go func() {
 		server.Serve(l)
 	}()
 
-	client = api.NewLogClient(cc)
 	return client, cfg, func() {
 		server.Stop()
 		cc.Close()
 		l.Close()
-		clog.Remove()
 	}
 }
 
