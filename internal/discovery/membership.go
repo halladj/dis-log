ec2f331ba4f844851630d1942ed53739782b6db2
halladj <halladj00@gmail.com>
2024-11-18 20:49:03 +0100

    Finished the service discovery feature

--- internal/discovery/membership.go
+++ internal/discovery/membership.go
@@ -106,6 +106,24 @@ func (m *Membership) isLocal(member serf.Member) bool {
 	return m.serf.LocalMember().Name == member.Name
 }
 
+func (m *Membership) Members() []serf.Member {
+	return m.serf.Members()
+}
+
+func (m *Membership) Leave() error {
+	return m.serf.Leave()
+}
+
+func (m *Membership) logError(
+	err error, msg string, member serf.Member) {
+	m.logger.Error(
+		msg,
+		zap.Error(err),
+		zap.String("name", member.Name),
+		zap.String("rpc_addr", member.Tags["rpc_addr"]),
+	)
+}
+
 type Config struct {
 	NodeName       string
 	BindAddr       string
