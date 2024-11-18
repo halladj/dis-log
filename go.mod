3460fc6462d92259fed9f99ddb1f30bcd3905929
halladj <halladj00@gmail.com>
2024-11-18 21:36:00 +0100

    tested service discovery + fixed go version requirements

--- go.mod
+++ go.mod
@@ -1,42 +1,22 @@
 module github.com/halladj/dis-log
 
-go 1.23.1
-
 require (
-	github.com/grpc-ecosystem/go-grpc-middleware v1.4.0
-	github.com/hashicorp/serf v0.10.1
-)
-
-require (
-	github.com/armon/go-metrics v0.0.0-20180917152333-f0300d1749da // indirect
-	github.com/google/btree v0.0.0-20180813153112-4030bb1f1f0c // indirect
-	github.com/hashicorp/errwrap v1.0.0 // indirect
-	github.com/hashicorp/go-immutable-radix v1.0.0 // indirect
-	github.com/hashicorp/go-msgpack v0.5.3 // indirect
-	github.com/hashicorp/go-multierror v1.1.0 // indirect
-	github.com/hashicorp/go-sockaddr v1.0.0 // indirect
-	github.com/hashicorp/golang-lru v0.5.0 // indirect
-	github.com/hashicorp/memberlist v0.5.0 // indirect
-	github.com/miekg/dns v1.1.41 // indirect
-	github.com/sean-/seed v0.0.0-20170313163322-e2103e2c3529 // indirect
-	go.uber.org/multierr v1.10.0 // indirect
-)
-
-require (
-	github.com/Knetic/govaluate v3.0.1-0.20171022003610-9aa49832a739+incompatible // indirect
+	github.com/armon/go-metrics v0.0.0-20190430140413-ec5e00d3c878 // indirect
 	github.com/casbin/casbin v1.9.1
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/golang/protobuf v1.4.1 // indirect
 	github.com/gorilla/mux v1.8.1
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/testify v1.9.0
+	github.com/grpc-ecosystem/go-grpc-middleware v1.1.0
+	github.com/hashicorp/go-msgpack v0.5.5 // indirect
+	github.com/hashicorp/serf v0.8.5
+	github.com/stretchr/testify v1.4.0
+	github.com/travisjeffery/go-dynaport v0.0.0-20171218080632-f8768fb615d5
 	github.com/tysonmote/gommap v0.0.3
-	go.uber.org/zap v1.27.0
-	golang.org/x/net v0.0.0-20210410081132-afb366fc7cd1 // indirect
-	golang.org/x/sys v0.0.0-20220728004956-3c1f35247d10 // indirect
-	golang.org/x/text v0.3.6 // indirect
+	go.uber.org/zap v1.10.0
+	golang.org/x/net v0.0.0-20190724013045-ca1201d0de80 // indirect
+	golang.org/x/sys v0.0.0-20190726091711-fc99dfbffb4e // indirect
+	golang.org/x/text v0.3.2 // indirect
 	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013
 	google.golang.org/grpc v1.32.0
 	google.golang.org/protobuf v1.25.0
-	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
+
+go 1.13
--- internal/discovery/membership.go
+++ internal/discovery/membership.go
@@ -1,10 +1,12 @@
 package discovery
 
+//hey
 import (
 	"net"
 
-	"github.com/hashicorp/serf/serf"
 	"go.uber.org/zap"
+
+	"github.com/hashicorp/serf/serf"
 )
 
 type Membership struct {
@@ -21,21 +23,24 @@ func New(handler Handler, config Config) (*Membership, error) {
 		handler: handler,
 		logger:  zap.L().Named("membership"),
 	}
-
 	if err := c.setupSerf(); err != nil {
 		return nil, err
 	}
-
 	return c, nil
 }
 
-func (m Membership) setupSerf() (err error) {
-	addr, err := net.ResolveTCPAddr(
-		"tcp", m.BindAddr)
+type Config struct {
+	NodeName       string
+	BindAddr       string
+	Tags           map[string]string
+	StartJoinAddrs []string
+}
+
+func (m *Membership) setupSerf() (err error) {
+	addr, err := net.ResolveTCPAddr("tcp", m.BindAddr)
 	if err != nil {
 		return err
 	}
-
 	config := serf.DefaultConfig()
 	config.Init()
 	config.MemberlistConfig.BindAddr = addr.IP.String()
@@ -43,19 +48,14 @@ func (m Membership) setupSerf() (err error) {
 	m.events = make(chan serf.Event)
 	config.EventCh = m.events
 	config.Tags = m.Tags
-	config.NodeName = m.NodeName
+	config.NodeName = m.Config.NodeName
 	m.serf, err = serf.Create(config)
-
 	if err != nil {
 		return err
 	}
-
 	go m.eventHandler()
 	if m.StartJoinAddrs != nil {
-		_, err = m.serf.Join(
-			m.StartJoinAddrs,
-			true,
-		)
+		_, err = m.serf.Join(m.StartJoinAddrs, true)
 		if err != nil {
 			return err
 		}
@@ -63,10 +63,14 @@ func (m Membership) setupSerf() (err error) {
 	return nil
 }
 
-func (m Membership) eventHandler() {
+type Handler interface {
+	Join(name, addr string) error
+	Leave(name string) error
+}
+
+func (m *Membership) eventHandler() {
 	for e := range m.events {
 		switch e.EventType() {
-
 		case serf.EventMemberJoin:
 			for _, member := range e.(serf.MemberEvent).Members {
 				if m.isLocal(member) {
@@ -77,7 +81,7 @@ func (m Membership) eventHandler() {
 		case serf.EventMemberLeave, serf.EventMemberFailed:
 			for _, member := range e.(serf.MemberEvent).Members {
 				if m.isLocal(member) {
-					continue
+					return
 				}
 				m.handleLeave(member)
 			}
@@ -98,7 +102,7 @@ func (m *Membership) handleLeave(member serf.Member) {
 	if err := m.handler.Leave(
 		member.Name,
 	); err != nil {
-		m.logError(err, "failde to leave", member)
+		m.logError(err, "failed to leave", member)
 	}
 }
 
@@ -114,23 +118,11 @@ func (m *Membership) Leave() error {
 	return m.serf.Leave()
 }
 
-func (m *Membership) logError(
-	err error, msg string, member serf.Member) {
+func (m *Membership) logError(err error, msg string, member serf.Member) {
 	m.logger.Error(
 		msg,
 		zap.Error(err),
 		zap.String("name", member.Name),
 		zap.String("rpc_addr", member.Tags["rpc_addr"]),
 	)
-}
-
-type Config struct {
-	NodeName       string
-	BindAddr       string
-	Tags           map[string]string
-	StartJoinAddrs []string
-}
-type Handler interface {
-	Join(name, addr string) error
-	Leave(name string) error
 }
