31779216632c3350c5be461ea3017cc7470761a0
halladj <halladj00@gmail.com>
2025-02-18 20:57:21 +0100

    added service discovery -for load balancing

--- internal/server/server.go
+++ internal/server/server.go
@@ -18,8 +18,28 @@ type Authorizer interface {
 }
 
 type Config struct {
-	CommitLog  CommitLog
-	Authorizer Authorizer
+	CommitLog   CommitLog
+	Authorizer  Authorizer
+	GetServerer GetServerer
+}
+
+func (s *grpcServer) GetServers(
+	ctx context.Context,
+	req **api.GetServersRequest,
+) (*api.GetServersResponse, error) {
+
+	servers, err := s.GetServerer.GetServers()
+	if err != nil {
+		return nil, err
+	}
+
+	return &api.GetServersResponse{
+		Servers: servers}, nil
+
+}
+
+type GetServerer interface {
+	GetServers() ([]*api.Server, error)
 }
 
 const (
